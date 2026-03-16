72b7d9eb2c64786a5ea9a9210e89ff2d2e5d8285
GeetBidwai <geetbidwai0102@gmail.com>
2026-03-16 11:07:13 +0000

    Remove employee_id from Staff model

--- backend/staff/models.py
+++ backend/staff/models.py
@@ -1,9 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
-
 
 class Staff(AbstractUser):
-    employee_id = models.CharField(max_length=20, unique=True)
-
     def __str__(self):
         return self.username
